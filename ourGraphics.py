91ef0d3c0486f76d4c08b5b9dd417c26eebc679e
Eric Niyazov <ERIC@Margaritas-iMac.home>
2019-04-04 19:36:54 -0400

    changed the rectangles into fuctions and change the color of one

--- ourGraphics.py
+++ ourGraphics.py
@@ -1,16 +1,19 @@
 from graphics import *
 
+
+def draw_rect(X1, Y1, sizeX, sizeY, color, win):
+    scrn = Rectangle(Point(X1, Y1), Point(X1 + sizeX, Y1 + sizeY))
+    scrn.setFill(color)
+    scrn.draw(win)
+
+
 grwin = GraphWin("ourGraphics.py", 1500, 1000)
 grwin.setCoords(0, 0, 1000, 1000)
 
 
-scrn1 = Rectangle(Point(230, 500), Point(230 + 440, 500 + 433))
-scrn1.setFill("dark blue")
-scrn1.draw(grwin)
-
-scrn = Rectangle(Point(250, 500), Point(250 + 400, 500 + 403))
-scrn.setFill("grey")
-scrn.draw(grwin)
-
-
 draw_rect(230, 500, 440, 433, "dark blue", grwin)
+
+draw_rect(250, 500, 400, 403, "black", grwin)
+
+
+
