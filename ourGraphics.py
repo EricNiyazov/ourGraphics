60ced773a0ee154dc6740a9fe3c578af5da8052d
Eric Niyazov <ERIC@Margaritas-iMac.home>
2019-04-04 20:30:42 -0400

    added a light blue screen and added 4 rectangles to be like a windows logo

--- ourGraphics.py
+++ ourGraphics.py
@@ -5,7 +5,11 @@ def draw_rect(X1, Y1, sizeX, sizeY, color, win):
     scrn = Rectangle(Point(X1, Y1), Point(X1 + sizeX, Y1 + sizeY))
     scrn.setFill(color)
     scrn.draw(win)
-
+    
+def draw_trap(X1,Y1, sizeX, sizeY, scale, color,win):
+    trap = Polygon(Point(X1, Y1), Point(X1+scale, Y1+sizeY), Point(X1+sizeX - scale, Y1+sizeY), Point(X1 + sizeX, Y1))
+    trap.setFill(color)
+    trap.draw(win)
 
 grwin = GraphWin("ourGraphics.py", 1500, 1000)
 grwin.setCoords(0, 0, 1000, 1000)
@@ -16,11 +20,14 @@ draw_rect(230, 500, 440, 433, "dark blue", grwin)
 draw_rect(250, 500, 400, 403, "black", grwin)
 
 
-trap = Polygon(Point(180, 105), Point(180+50, 105+395), Point(180+540 - 50, 105+395), Point(180 + 540, 105))
-trap.setFill("black")
-trap.draw(grwin)
+draw_trap(180, 105, 540, 395, 50, "black", grwin) 
 
-trap = Polygon(Point(200, 125), Point(200+50, 125+375), Point(200+500 - 50, 125+375), Point(200 + 500, 125))
-trap.setFill("grey")
-trap.draw(grwin)
+draw_trap(200,125,500, 375, 50, "grey", grwin)
+
+draw_rect(255, 505, 390, 392, "light blue", grwin)
+
+draw_rect(300, 700, 75, 100, "red", grwin)
+draw_rect(300, 590, 75, 100, "blue", grwin)
+draw_rect(385, 700, 75, 100, "green", grwin)
+draw_rect(385, 590, 75, 100, "yellow", grwin)
 
