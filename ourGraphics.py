302a3fe43908a10d6c2562087ea02b914de0649b
Eric Niyazov <ERIC@Margaritas-iMac.home>
2019-04-05 17:56:24 -0400

    finished added letters

--- ourGraphics.py
+++ ourGraphics.py
@@ -16,6 +16,17 @@ def draw_key(X1,Y1, sizeX, sizeY, scale, letter, color,win):
     key = Text(Point(X1 + sizeX / 2, Y1 + sizeY / 2), letter)
     key.draw(win)
 
+##def alphabet_position(text):
+##    letr = ('a'='1','b':'2','c':'3','d':'4','e':'5','f':'6','g':'7','h':'8',
+##    'i':'9','j':'10','k':'11','l':'12','m':'13','n':'14','o':'15','p':'16','q':'17',
+##    'r':'18','s':'19','t':'20','u':'21','v':'22','w':'23','x':'24','y':'25','z':'26')
+
+ABC = ["1","2","3","4","5","6","7","8","9","0","-","Q","W","E","R"
+       ,"T","Y","U","I","O","P","[","A","S","D","F","G","H",
+       "J","K","L",";","'","Z","X","C","V","B","N","M",",",".","/","sft"
+       ]
+
+
 grwin = GraphWin("ourGraphics.py", 1500, 1000)
 grwin.setCoords(0, 0, 1000, 1000)
 
@@ -35,7 +46,6 @@ draw_rect(300, 700, 75, 100, "red", grwin)
 draw_rect(300, 590, 75, 100, "blue", grwin)
 draw_rect(385, 700, 75, 100, "green", grwin)
 draw_rect(385, 590, 75, 100, "yellow", grwin)
-<<<<<<< .merge_file_OtDEw6
 
 logo = Text(Point(450, 485), "banana")
 logo.setText("BANANA")
@@ -44,6 +54,34 @@ logo.setSize(30)
 logo.setFace("arial")
 logo.draw(grwin)
 
-=======
->>>>>>> .merge_file_bsh3yM
+chCol = 230
+
+kX = 250
+kY = 470
+#letrs = 1
+col = "white"
+c = 0
+
+print(ABC[3])
+
+
+for j in range (4):
+    kY -= 40
+    kX = 250
+    
+    for i in range (11):
+        kX += 30
+        draw_key(kX, kY, 30, 37.5, 5, ABC[c], col, grwin)
+        print(kX)
+        print(kY)
+        c += 1
+        if (i + j) % 2 == 0:
+            col = "dark grey"
+        else:
+            col = "white"
+
+            
+draw_key(400, 250, 100, 37.5, 5, "SPACE","white", grwin)
+
+draw_trap(400, 130, 100, 100, 5, "light grey", grwin)
 
