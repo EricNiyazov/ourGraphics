74721a78354470bd8d7b3ac25fa9328a1147b7cb
Eric Niyazov <ERIC@Margaritas-iMac.home>
2019-04-04 20:34:22 -0400

    added a fuction for keys on the keyboard

--- ourGraphics.py
+++ ourGraphics.py
@@ -11,6 +11,11 @@ def draw_trap(X1,Y1, sizeX, sizeY, scale, color,win):
     trap.setFill(color)
     trap.draw(win)
 
+def draw_key(X1,Y1, sizeX, sizeY, scale, letter, color,win):
+    key = draw_trap(X1,Y1, sizeX, sizeY, scale, color,win)
+    key = Text(Point(X1 + sizeX / 2, Y1 + sizeY / 2), letter)
+    key.draw(win)
+
 grwin = GraphWin("ourGraphics.py", 1500, 1000)
 grwin.setCoords(0, 0, 1000, 1000)
 
@@ -23,3 +28,11 @@ draw_rect(250, 500, 400, 403, "black", grwin)
 draw_trap(180, 105, 540, 395, 50, "black", grwin) 
 
 draw_trap(200,125,500, 375, 50, "grey", grwin)
+
+draw_rect(255, 505, 390, 392, "light blue", grwin)
+
+draw_rect(300, 700, 75, 100, "red", grwin)
+draw_rect(300, 590, 75, 100, "blue", grwin)
+draw_rect(385, 700, 75, 100, "green", grwin)
+draw_rect(385, 590, 75, 100, "yellow", grwin)
+
