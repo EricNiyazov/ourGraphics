376ed3299e4eb309a8005e9850f3039bc3b58f85
Eric Niyazov <ERIC@Margaritas-iMac.home>
2019-04-04 20:36:23 -0400

    added a BANANA brand logo

--- ourGraphics.py
+++ ourGraphics.py
@@ -11,6 +11,11 @@ def draw_trap(X1,Y1, sizeX, sizeY, scale, color,win):
     trap.setFill(color)
     trap.draw(win)
 
+def draw_key(X1,Y1, sizeX, sizeY, scale, letter, color,win):
+    key = draw_trap(X1,Y1, sizeX, sizeY, scale, color,win)
+    key = Text(Point(X1 + sizeX / 2, Y1 + sizeY / 2), letter)
+    key.draw(win)
+
 grwin = GraphWin("ourGraphics.py", 1500, 1000)
 grwin.setCoords(0, 0, 1000, 1000)
 
@@ -30,4 +35,12 @@ draw_rect(300, 700, 75, 100, "red", grwin)
 draw_rect(300, 590, 75, 100, "blue", grwin)
 draw_rect(385, 700, 75, 100, "green", grwin)
 draw_rect(385, 590, 75, 100, "yellow", grwin)
+
+logo = Text(Point(450, 485), "banana")
+logo.setText("BANANA")
+logo.setTextColor("green")
+logo.setSize(30)
+logo.setFace("arial")
+logo.draw(grwin)
+
 
